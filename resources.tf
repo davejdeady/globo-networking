92b08eddc31f7d7f39b010d57f86eb802d4e4e0e
davejdeady <davidjohndeady@outlook.com>
2025-09-19 16:17:04 +0100

    Add workspace tag and make variables required

--- resources.tf
+++ resources.tf
@@ -19,7 +19,7 @@ locals {
   common_tags = {
     Enviroment  = var.environment
     BillingCode = var.billing_code
-    Workspace = terraform.workspace
+    Workspace   = terraform.workspace
 
   }
 }
--- variables.tf
+++ variables.tf
@@ -11,13 +11,13 @@ variable "region" {
 variable "prefix" {
   type        = string
   description = "(Optional) Prefix to use for all resources in this module. Default: globo-dev"
-  
+
 }
 
 variable "environment" {
   type        = string
   description = "optional environment of all resources"
-  
+
 }
 
 variable "billing_code" {
@@ -28,12 +28,12 @@ variable "billing_code" {
 variable "cidr_block" {
   type        = string
   description = "(Optional) The CIDR block for the VPC. Default:10.42.0.0/16"
-  
+
 }
 
 variable "public_subnets" {
   type        = map(string)
   description = "(Optional) Map of public subnets to create with CIDR blocks. Key will be used as subnet name with prefix. Default: {subnet-1 ="
-  
+
 }
 
