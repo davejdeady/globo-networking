4014b89fad5f5abc65f8bc7b7d03f9360ce5e4eb
davejdeady <davidjohndeady@outlook.com>
2025-09-19 09:09:01 +0100

    Add third subnet and tags

--- resources.tf
+++ resources.tf
@@ -17,7 +17,8 @@ data "aws_availability_zones" "available" {}
 ##################################################################################
 locals {
   common_tags = {
-    Enviroment = var.environment
+    Enviroment  = var.environment
+    BillingCode = var.billing_code
 
   }
 }
