8a2060a5341bad26b379e8efe1c8dc44671b1058
davejdeady <davidjohndeady@outlook.com>
2025-10-02 10:48:53 +0100

    updating with owner

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ variable "environment" {
 variable "billing_code" {
   type        = string
   description = "billing code for network resources"
-  default     = "124213"
+  default     = "121213"
 }
 
 
